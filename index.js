ac371b8ac1fd94611e088c5e76508c18f9da2328
kagure-nyakio <kagurenyakio@gmail.com>
2022-03-28 14:12:29 +0300

    code format

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Fetch advise from API
 // Render Advise
-const getQuoteBtn = document.querySelector('#get-quote')
-const quoteTitleEl = document.querySelector('.quote-title')
-const quoteTextEl = document.querySelector('.quote-text')
+const getQuoteBtn   = document.querySelector('#get-quote')
+const quoteTitleEl  = document.querySelector('.quote-title')
+const quoteTextEl   = document.querySelector('.quote-text')
 
 
 getQuoteBtn.addEventListener('click',  () => {
   fetch('https://api.adviceslip.com/advice')
     .then(res => res.json())
     .then(advice => {
-      quoteTitleEl.textContent = `Advice #${advice.slip.id}`
-      quoteTextEl.innerHTML = `<q>${advice.slip.advice}</q>`
+      quoteTitleEl.textContent  = `Advice #${advice.slip.id}`
+      quoteTextEl.innerHTML     = `<q>${advice.slip.advice}</q>`
     })
 })
